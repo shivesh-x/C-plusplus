f2bb0eefb818bc5d97dc02319b59c40e16d8de20
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2019-10-12 12:25:40 +0530

    Update Sum of N Number.cpp

--- Loops/Sum of N Number.cpp
+++ Loops/Sum of N Number.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	int i,sum=0,n;                                                 // The answer can also be checked by using the mathematical formula for
-	cout<<"Number till where sum is prinnted:";                     // sum of n natural number i.e. n*(n+1)/2;
+	cout<<"Number till where sum is printed:";                     // sum of n natural number i.e. n*(n+1)/2;
 	cin>>n;
 	for(i=1;i<=n;i++)
 	{
