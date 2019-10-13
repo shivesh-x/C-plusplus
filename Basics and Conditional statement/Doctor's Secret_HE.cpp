2c029ef619a8d869569c683da468dfdda07a191b
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2019-10-14 00:22:49 +0530

    Create Doctor's Secret_HE.cpp

---
+++ Basics and Conditional statement/Doctor's Secret_HE.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l,s;
+	cin>>l>>s;
+	if((l<=23)&&(s>500&&s<=1000))
+		cout<<"Take Medicine";
+	else
+		cout<< "Don't take Medicine";
+		
+}
