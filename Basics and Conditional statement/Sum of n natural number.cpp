ad01797f0c4ba5935edf7cb8da1bcd212f181e60
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2019-11-02 06:13:09 +0530

    Create Sum of n natural number.cpp

---
+++ Basics and Conditional statement/Sum of n natural number.cpp
@@ -0,0 +1,12 @@
+//Sum of First n natural number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cin>>n;
+    sum=(n*(n+1))/2;
+    cout<<"Sum: "<<sum;
+    return 0;
+
+}
