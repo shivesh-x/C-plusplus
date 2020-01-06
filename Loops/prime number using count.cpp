286fd0f29884c0fe0f2c7dc6ad6a6671b815347d
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2020-01-07 01:13:06 +0530

    Create prime number using count.cpp

---
+++ Loops/prime number using count.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,count=0;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+
+    }
+
+    if(count==2)
+    {
+        cout<<n<<" is a prime number";
+    }
+    else
+        cout<<"not a Prime number";
+    
+    return 0;
+}
