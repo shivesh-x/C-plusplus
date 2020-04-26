4311f98a44cca09bce2ea1243abd16191fc0c723
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2020-04-27 01:36:33 +0530

    Create Finding maximum of two numbers.cpp

---
+++ Basics and Conditionals/Finding maximum of two numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter two numbers :";
+    cin>>x>>y;
+    if(x>y)
+    {
+        cout<<x<<" is greater than "<<y;
+    }
+    else
+    {
+        cout<<y<<" is greater than "<<x;
+
+    }
+}
