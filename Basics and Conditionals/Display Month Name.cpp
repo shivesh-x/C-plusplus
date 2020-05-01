f527e97e995faa20787406d3e00fdd77310edf1b
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2020-05-01 08:40:43 +0530

    Create Display Month Name.cpp

---
+++ Basics and Conditionals/Display Month Name.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+void MonthName(int m)
+{
+    if( m == 1)
+        cout<<"jan";
+    else if( m == 2)
+        cout<<"feb";
+    else if( m == 3)
+        cout<<"mar";
+    else if( m == 4)
+        cout<<"apr";
+    else if( m == 5)
+        cout<<"may";
+    else if( m == 6)
+        cout<<"jun";
+    else if( m == 7)
+        cout<<"jul";
+    else if( m == 8)
+        cout<<"aug";
+    else if( m == 9)
+        cout<<"sep";
+    else if( m == 10)
+        cout<<"oct";
+    else if( m == 11)
+        cout<<"nov";
+    else
+        cout<<"dec";
+    
+}
+    
