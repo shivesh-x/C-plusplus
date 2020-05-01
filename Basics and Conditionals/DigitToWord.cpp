b0befbe0e03dac46d1e5e53611e2de46d7051cf5
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2020-05-01 08:53:53 +0530

    Create DigitToWord.cpp

---
+++ Basics and Conditionals/DigitToWord.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int d;
+    cout<<"Enter Digit: ";
+    cin>>d;
+    if( d == 1)
+        cout<<"one";
+    else if( d == 2)
+        cout<<"two";
+    else if( d == 3)
+        cout<<"three";
+    else if( d == 4)
+        cout<<"four";
+    else if( d == 5)
+        cout<<"five";
+    else if( d == 6)
+        cout<<"six";
+    else if( d == 7)
+        cout<<"seven";
+    else if( d == 8)
+        cout<<"eight";
+    else if( d == 9)
+        cout<<"nine";
+    else
+        cout<<"zero";
+
+    return 0;
+}
