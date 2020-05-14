b582385a3da327c2e0fcb30bc4863e2340328f25
Arindam Priyadarshi <51259879+legendphoenix0@users.noreply.github.com>
2020-05-15 04:42:13 +0530

    Update Check For Armstrong Number.cpp

--- Loops/Check For Armstrong Number.cpp
+++ Loops/Check For Armstrong Number.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-    int n,r,num,a=0;
+    int n,r,num,sum=0;
     cin>>n;
     num=n;
     while(n>0)
     {
         r=n%10;
         n=n/10;
-        a=a+(r*r*r);
+        sum=sum+(r*r*r);
     }
-    if(num == a)
+    if(sum == num)
         cout<<"Armstrong Number";
     else
         cout<<"Not a Armstrong Number";
